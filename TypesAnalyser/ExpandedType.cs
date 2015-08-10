eba8dc1b8dc9aea8fe833f53291c61abf4c41c8c
Arturas Slajus <x11@arturaz.net>
2015-08-10 11:22:15 +0300

    Initial commit.

---
+++ TypesAnalyser/ExpandedType.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+using Mono.Cecil;
+
+namespace TypesAnalyser {
+  /* Type which is guaranteed to be non-generic and has all of its generic arguments expanded. */
+  struct ExpandedType {
+    public static readonly ImmutableList<ExpandedType> EMPTY_GENERIC_PARAMETERS =
+      ImmutableList<ExpandedType>.Empty;
+
+    public readonly TypeDefinition definition;
+    public readonly ImmutableList<ExpandedType> genericParameters;
+
+    private readonly com.tinylabproductions.TLPLib.Functional.Lazy<string> _name;
+    public string name => _name.get;
+
+    public ExpandedType(
+      TypeDefinition definition,
+      ImmutableList<ExpandedType> genericParameters
+    ) : this() {
+      this.definition = definition;
+      this.genericParameters = genericParameters;
+      _name = F.lazy(() => {
+        var sb = new StringBuilder();
+        sb.Append(definition.FullName);
+        if (!genericParameters.isEmpty()) sb.Append(genericParameters.mkString(",", "<", ">"));
+        return sb.ToString();
+      });
+    }
+
+    #region Equality
+
+    public bool Equals(ExpandedType other) {
+      return Equals(_name, other._name);
+    }
+
+    public override bool Equals(object obj) {
+      if (ReferenceEquals(null, obj)) return false;
+      return obj is ExpandedType && Equals((ExpandedType) obj);
+    }
+
+    public override int GetHashCode() {
+      return _name?.GetHashCode() ?? 0;
+    }
+
+    #endregion
+
+    public override string ToString() { return _name.get; }
+
+    public static Option<ExpandedType> a(TypeDefinition definition) {
+      if (definition.IsGenericParameter || definition.IsGenericInstance) return F.none<ExpandedType>();
+      return F.some(new ExpandedType(definition, EMPTY_GENERIC_PARAMETERS));
+    }
+
+    public static ExpandedType create(TypeDefinition definition) {
+      return a(definition).
+        getOrThrow(() => new Exception("Expected " + definition + " to be non-generic"));
+    }
+
+    public static ExpandedType create(
+      TypeReference reference, IDictionary<GenericParameter, ExpandedType> generics
+    ) {
+      var definition = reference.Resolve();
+      if (reference.IsGenericInstance) {
+        var gRef = (GenericInstanceType) reference;
+        var parameters = gRef.GenericArguments.Select(arg => 
+          generics[(GenericParameter) arg]
+        ).ToImmutableList();
+        return new ExpandedType(definition, parameters);
+      }
+      if (reference.IsGenericParameter) {
+        var gParam = (GenericParameter) reference;
+        return generics[gParam];
+      }
+      return create(definition);
+    }
+  }
+}
---
+++ TypesAnalyser/UnityEntryPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+using Mono.Cecil;
+
+namespace TypesAnalyser {
+  struct UnityEntryPoint {
+    public readonly ExpandedType type;
+
+    public UnityEntryPoint(ExpandedType type) {
+      this.type = type;
+    }
+
+    public override string ToString() {
+      return $"UnityEntryPoint[{type}]";
+    }
+
+    static bool isEntryPoint(TypeDefinition type) {
+      return type.FullName == "UnityEngine.MonoBehaviour" ||
+        (type.BaseType != null && isEntryPoint(type.BaseType.Resolve()));
+    }
+
+    public static Option<UnityEntryPoint> create(TypeDefinition type) {
+      return isEntryPoint(type).opt(() => new UnityEntryPoint(ExpandedType.create(type)));
+    }
+  }
+}
